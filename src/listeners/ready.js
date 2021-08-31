bcfc1c2f99787c4af3c246ce089aa953fe157e6d
ErrorDoc404 <akashsinhakvs@gmail.com>
2021-08-31 11:56:12 +0530

    Update ready.js

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,6 +1,13 @@
 module.exports = {
     name: "ready",
     exec: async (client) => {
+      client.user.setPresence({
+        status: "online",  // You can show online, idle, and dnd
+        activity: {
+            name: "Help",  // The message shown
+            type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+      });
         console.log(`Logged in as ${client.user.tag}`);
 
         if (client.spotify) await client.spotify.requestToken();
